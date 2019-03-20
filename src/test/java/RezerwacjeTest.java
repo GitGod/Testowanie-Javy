45d8eedd1c505b18220b6d963834425331eeea98
GitGod <barmazu23@gmail.com>
2019-03-20 19:55:27 +0100

    Add Hamcrest

--- src/test/java/RezerwacjeTest.java
+++ src/test/java/RezerwacjeTest.java
@@ -1,17 +1,16 @@
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
-import static org.assertj.core.api.Assertions.*;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-
 import java.io.IOException;
 import java.util.ArrayList;
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 
 
@@ -76,6 +75,15 @@ public class RezerwacjeTest {
     public void TestRegexImieTrue() {
         assertTrue(rezerwacje.validOsoba(osoba1));
     }
+     @Test
+    public void TestGetEmail() {
+        assertThat(osoba1.getEmail(), is("damian@o2.pl"));
+    }
+    @Test
+    public void TestGetEmailNot() {
+        assertThat(osoba1.getEmail(), not("damia@o2.pl"));
+    }
+
 
 
   
