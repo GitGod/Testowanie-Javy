1df77cefbb0761e3c4c7e7f8b38305f2deb88812
GitGod <barmazu23@gmail.com>
2019-03-27 16:32:45 +0100

    Update Rezerwacja

--- src/main/java/Rezerwacja.java
+++ src/main/java/Rezerwacja.java
@@ -1,19 +1,24 @@
+import java.time.LocalDate;
 import java.util.Date;
 
 public class Rezerwacja {
     public String dzien;
+    public LocalDate data;
     public int godzina;
+    public int godzinaDo;
     public Stolik stolik;
 
-    public Rezerwacja(String dzien, int godzina, Stolik stolik) {
-
+    public Rezerwacja(LocalDate data, int godzina, int godzinaDo, Stolik stolik) {
+        this.data = data;
         this.dzien = dzien;
         this.godzina = godzina;
+        this.godzinaDo = godzinaDo;
         this.stolik = stolik;
     }
 
-   @Override
+    @Override
     public String toString() {
         return "Stolik numer " + stolik.numer + " " + stolik.liczbaMiejsc + " osobowy " + data + " godzina " + godzina + "-" + godzinaDo;
     }
 }
+
