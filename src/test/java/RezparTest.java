0fd6b740cc16ee24e5de8ec3e3de716bb2811fb3
GitGod <barmazu23@gmail.com>
2019-03-31 11:41:03 +0200

    Update tests

--- src/test/java/RezparTest.java
+++ src/test/java/RezparTest.java
@@ -8,13 +8,11 @@ import javax.mail.MessagingException;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Arrays;
-import java.util.Date;
 
-import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(Parameterized.class)
-public class RezparTest extends TestBazowy{
+public class RezparTest extends BazeTest {
     @Parameterized.Parameter(0)
     public Rezerwacja fInput;
     @Parameterized.Parameter(1)
@@ -25,10 +23,10 @@ public class RezparTest extends TestBazowy{
         Stolik s1 = new Stolik(1, 4);
         Stolik s2 = new Stolik(2, 3);
         return Arrays.asList(new Object[][]{
-                {new Rezerwacja(LocalDate.of(2019, 1, 17), "12.30", "14", s1), false},
-                {new Rezerwacja(LocalDate.of(2019, 1, 17), "12", "14", s2), true},
-                {new Rezerwacja(LocalDate.of(2019, 1, 17), "15", "16", s1), true},
-                {new Rezerwacja(LocalDate.of(2019, 1, 17), "15", "16", s2), true},
+                {new Rezerwacja(LocalDate.of(2022, 1, 17), "12.30", "14", s1), false},
+                {new Rezerwacja(LocalDate.of(2022, 1, 17), "12", "14", s2), true},
+                {new Rezerwacja(LocalDate.of(2022, 1, 17), "15", "16", s1), true},
+                {new Rezerwacja(LocalDate.of(2022, 1, 17), "15", "16", s2), true},
         });
     }
 
@@ -39,7 +37,7 @@ public class RezparTest extends TestBazowy{
         Stolik s1 = new Stolik(1, 4);
         restauracja = new Restauracja();
         restauracja.readFile("Restauracja.txt");
-        rezerwacje.dodajRezerwacje(new Osoba("Kamil", "a@o2.pl"), new Rezerwacja(LocalDate.of(2019, 1, 17), "12", "14", s1), restauracja);
+        rezerwacje.dodajRezerwacje(new Osoba("Kamil", "a@o2.pl"), new Rezerwacja(LocalDate.of(2022, 1, 17), "12", "14", s1), restauracja);
     }
 
     @Test
@@ -54,7 +52,6 @@ public class RezparTest extends TestBazowy{
         restauracja = null;
     }
 }
-
 
 
 
